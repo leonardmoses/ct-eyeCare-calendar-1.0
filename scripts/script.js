ca2368bb582d75fec6bd2d2892961cf5f768922c
Qalexeon <qalexeon@Xavier.lan>
2022-10-03 20:32:44 -0400

    In JS set 1st day of month to start on corrent day of week. Display last days of previous month. Display first days of next month. Set current date.

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,38 @@ const date = new Date();
 // get current month (index number) from built in date method
 const month = date.getMonth();
 
+// // Testing: change index num of the month to test last day of the month.
+// date.setMonth(10);
+
+// set the date to the first day of the current Month
+date.setDate(1);
+// check(in console) which day of the week is the first day of month.
+console.log(date.getDay()+": Index of 1st day of month");
+
+// get class for days of month. Used later to display days of month.
 const monthDays =document.querySelector('.days');
+
+// get last date of the current month
+const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+const firstDayIndex = date.getDay();
+
+// check which day of the week is the last day of month.
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth() +1, 0).getDay();
+console.log(lastDayIndex)
+
+// count how many days come after the last day of month.
+const nextDays = 7 - lastDayIndex - 1;
+console.log(nextDays)
 
 // list of months to convert index number to written format
 const months = [
     "January",
     "February",
     "March",
-    "April",
+    "April", 
     "May",
     "June",
     "July",
@@ -30,10 +54,21 @@ document.querySelector('.date p').innerHTML = date.toDateString()
 
 let days = "";
 
-for(let i = 1; i<=31; i++) {
+for(let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x +1}</div>`;
+}
 
-    // days += `<div>${i}</div>`
+for(let i = 1; i <= lastDay; i++) {
+    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+        days += `<div class="today">${i}</div>`;
+    } else {
+        days += `<div>${i}</div>`;
+    }
+
     // monthDays.innerHTML = days;
 }
 
-
+for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`
+    monthDays.innerHTML = days;
+}
