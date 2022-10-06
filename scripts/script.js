7cc905d107db7b493fbbd7a1265d7c14bcc2e0f7
Qalexeon <qalexeon@Xavier.lan>
2022-10-06 15:38:39 -0400

    Get current date/year and toggle between test year. Check if leap year.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,32 @@
+// Set date to current date
 const date = new Date();
 
+// #region Leap Year Check
+
+// const year = date.getFullYear()
+const year = 2020;
+console.log(`Current year: ${year}`)
+let isLeapYear = false;
+
+if (year % 4 === 0)  {
+    isLeapYear = true;
+} else if (year % 100 === 4) {
+    isLeapYear = true;
+} else {
+    isLeapYear = false;
+}
+console.log(`Leap Year: ${isLeapYear}`)
+
+let totalYearDays = 365;
+if (isLeapYear) {
+    totalYearDays = 366;
+};
+// #endregion
+
+
+console.log(`Total Days = ${totalYearDays}`)
+
+// array list of months
 const months = [
     "January",
     "February",
@@ -15,7 +42,10 @@ const months = [
     "December",
 ]
 
+// display Month in calHeader
 document.querySelector('.selectedMonth').innerHTML = months[date.getMonth()]
 
+// display date in calHeader
 document.querySelector('.selectedDate').innerHTML = date.toDateString();
+
 
